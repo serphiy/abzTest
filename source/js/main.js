8eed32752e2287044819a59c373beb4a24a78c4f
Serhii Ralchenko <ralchenkos@gmail.com>
2018-06-28 14:59:09 +0300

    added showModal function and modal style

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,51 @@
+function showModal(title, info, desc) {
+  var modal = document.createElement('div');
+  $(modal).addClass('modal');
+
+  var modalBox = document.createElement('div');
+  $(modalBox).addClass('modal__box')
+             .appendTo($(modal));
+
+  var modalHeader = document.createElement('header');
+  $(modalHeader).addClass('modal__header')
+                .appendTo($(modalBox));
+
+  var modalTitle = document.createElement('h2');
+  $(modalTitle).addClass('modal__title')
+               .text(title)
+               .appendTo($(modalHeader));
+
+  var modalClose = document.createElement('button');
+  $(modalClose).addClass('modal__close')
+               .text('\u00D7')
+               .appendTo($(modalHeader));
+
+  var modalContent = document.createElement('div');
+  $(modalContent).addClass('modal__content')
+                 .appendTo($(modalBox));
+
+  var modalInfo = document.createElement('h3');
+  $(modalInfo).addClass('modal__info')
+              .text(info)
+              .appendTo($(modalContent));
+
+  var modalDesc = document.createElement('p');
+  $(modalDesc).addClass('modal__desc')
+              .text(desc)
+              .appendTo($(modalContent));
+
+  var modalFooter = document.createElement('footer');
+  $(modalFooter).addClass('modal__footer')
+                .appendTo($(modalBox));
+
+  var modalOk = document.createElement('button');
+  $(modalOk).addClass('modal__ok')
+            .text('OK')
+            .appendTo($(modalFooter));
+
+  $(modal).appendTo($('body'));
+}
+
 function getToken() {
   var url_base = 'http://504080.com/api/v1/account/login';
   var requestPayload = {
@@ -41,31 +89,31 @@ function getServices(token) {
     });
 }
 
+function createServiceCard(icon, title) {
+  if (!(icon) || !(title)) {return;}
+
+  var card = document.createElement('a');
+  $(card).addClass('service-card')
+         .attr('href','#');
+
+  var cardImgWrapper = document.createElement('div');
+  $(cardImgWrapper).addClass('service-card__img-wrapper')
+               .appendTo($(card));
+
+  var cardImg = document.createElement('img');
+  $(cardImg).addClass('service-card__img')
+            .attr('src', icon)
+            .attr('alt', title)
+            .appendTo($(cardImgWrapper));
+
+  var cardDesc = document.createElement('div');
+  $(cardDesc).addClass('service-card__desc')
+               .text(title)
+               .appendTo($(card));
+  return $(card);
+}
+
 function renderServices(servicesArray) {
-  function createServiceCard(icon, title) {
-    if (!(icon) || !(title)) {return;}
-
-    var card = document.createElement('a');
-    $(card).addClass('service-card')
-           .attr('href','#');
-
-    var cardImgWrapper = document.createElement('div');
-    $(cardImgWrapper).addClass('service-card__img-wrapper')
-                 .appendTo($(card));
-
-    var cardImg = document.createElement('img');
-    $(cardImg).addClass('service-card__img')
-              .attr('src', icon)
-              .attr('alt', title)
-              .appendTo($(cardImgWrapper));
-
-    var cardDesc = document.createElement('div');
-    $(cardDesc).addClass('service-card__desc')
-                 .text(title)
-                 .appendTo($(card));
-    return $(card);
-  }
-
   var servicesElements = [];
   for (var i = 0; i < servicesArray.length; i++) {
     servicesElements.push(createServiceCard(servicesArray[i].icon, servicesArray[i].title));
